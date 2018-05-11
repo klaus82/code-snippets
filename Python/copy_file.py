bacd806286617507d8fa5d121e087267288308cc
claudio.masolo <claudio.masolo@siospa.it>
2018-05-11 15:08:53 +0200

    Fixed copy_file.py

--- Python/copy_file.py
+++ Python/copy_file.py
@@ -6,7 +6,7 @@ def copy_file(source, file_type, destination):
     files = [f for f in os.listdir(source) if os.path.isfile(os.path.join(source, f))]
     for f in files:
         if f.endswith(file_type):
-            shutil.copy(f,destination)
+            shutil.copy(os.path.join(source, f),destination)
             print("Copied:{0}".format(f))
 
 if __name__ == "__main__":
